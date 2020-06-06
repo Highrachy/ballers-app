6a36bcb3ef2c11dfd1ddb37789f5266086f6312a
Haruna Popoola <haruna.popoola@yahoo.com>
2020-06-06 08:32:03 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,35 @@
 import React from 'react';
+import axios from 'axios';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [message, setMessage] = React.useState(null);
+  React.useEffect(() => {
+    axios
+      .get('https://staging-ballers-api.herokuapp.com/api/v1/')
+      .then(function (response) {
+        const { status, data } = response;
+        console.log('status,data', status, data);
+        // handle success
+        if (status === 200) {
+          setMessage(data.message);
+        }
+      })
+      .catch(function (error) {
+        console.log(error.response.data.message);
+        setMessage([]);
+      });
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+          <br />
+          {message === null ? 'Loading...' : message}
         </p>
         <a
           className="App-link"
