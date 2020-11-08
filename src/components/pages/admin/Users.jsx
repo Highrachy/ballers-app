3895b3b80eb28e9aa102ab792460a152584defbc
Haruna Popoola <haruna.popoola@yahoo.com>
2020-11-08 21:12:11 +0100

    feature: upgrade and downgrade user to content editor

--- src/components/pages/admin/Users.jsx
+++ src/components/pages/admin/Users.jsx
@@ -12,7 +12,6 @@ import { UsersIcon } from 'components/utils/Icons';
 import { getError } from 'utils/helpers';
 import TopTitle from 'components/utils/TopTitle';
 import ProfileAvatar from 'assets/img/avatar/profile.png';
-import { Link } from '@reach/router';
 import { SuccessIcon, InfoIcon } from 'components/utils/Icons';
 
 const Users = () => {
@@ -59,31 +58,40 @@ const AllUsers = ({ users, toast }) => (
   </LoadItems>
 );
 
-const UsersRowList = ({ users }) => (
-  <div className="container-fluid">
-    <Card className="mt-4">
-      <div className="table-responsive">
-        <table className="table table-border table-hover">
-          <thead>
-            <tr>
-              <td>S/N</td>
-              <td>Avatar</td>
-              <td>Name</td>
-              <td>Phone</td>
-              <td>Status</td>
-              <td></td>
-            </tr>
-          </thead>
-          <tbody>
-            {users.map((user, index) => (
-              <UsersRow key={index} number={index + 1} {...user} />
-            ))}
-          </tbody>
-        </table>
-      </div>
-    </Card>
-  </div>
-);
+const UsersRowList = ({ users }) => {
+  const [toast, setToast] = useToast();
+  return (
+    <div className="container-fluid">
+      <Toast {...toast} showToastOnly />
+      <Card className="mt-4">
+        <div className="table-responsive">
+          <table className="table table-border table-hover">
+            <thead>
+              <tr>
+                <td>S/N</td>
+                <td>Avatar</td>
+                <td>Name</td>
+                <td>Phone</td>
+                <td>Status</td>
+                <td></td>
+              </tr>
+            </thead>
+            <tbody>
+              {users.map((user, index) => (
+                <UsersRow
+                  key={index}
+                  setToast={setToast}
+                  number={index + 1}
+                  {...user}
+                />
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </Card>
+    </div>
+  );
+};
 
 UsersRowList.propTypes = {
   users: PropTypes.array.isRequired,
@@ -99,42 +107,88 @@ const UsersRow = ({
   phone,
   phone2,
   profileImage,
-}) => (
-  <tr>
-    <td>{number}</td>
-    <td>
-      <img
-        alt={firstName}
-        className="img-fluid avatar--medium--small"
-        src={profileImage ? profileImage.url : ProfileAvatar}
-        title={firstName}
-      />
-    </td>
-    <td>
-      {firstName} {lastName} <br />
-      <small>{email}</small>
-    </td>
-    <td>
-      {phone} <br />
-      {phone2 || '-'}
-    </td>
-    <td>
-      {activated ? (
-        <span className="text-green">
-          <SuccessIcon />{' '}
-        </span>
-      ) : (
-        <span className="text-muted">
-          <InfoIcon />
-        </span>
-      )}
-    </td>
-    <td>
-      <Link className="btn btn-sm btn-secondary" to={`/admin/user/${_id}`}>
-        View User
-      </Link>
-    </td>
-  </tr>
-);
+  role,
+  setToast,
+}) => {
+  const userId = _id;
+  const [userRole, setUserRole] = React.useState(role);
+  const processRoleChange = () => {
+    const action = userRole === 1 ? 'upgrade' : 'downgrade';
+
+    Axios.put(
+      `${BASE_API_URL}/user/editor/${action}`,
+      { userId },
+      {
+        headers: { Authorization: getTokenFromStore() },
+      }
+    )
+      .then(function (response) {
+        const { status, data } = response;
+        if (status === 200) {
+          setToast({
+            message: data.message,
+            type: 'success',
+          });
+          setUserRole(userRole === 1 ? 3 : 1);
+        }
+      })
+      .catch(function (error) {
+        setToast({
+          message: getError(error),
+        });
+      });
+  };
+  return (
+    <tr>
+      <td>{number}</td>
+      <td>
+        <img
+          alt={firstName}
+          className="img-fluid avatar--medium--small"
+          src={profileImage ? profileImage.url : ProfileAvatar}
+          title={firstName}
+        />
+      </td>
+      <td>
+        {firstName} {lastName} <br />
+        <small>{email}</small>
+      </td>
+      <td>
+        {phone} <br />
+        {phone2 || '-'}
+      </td>
+      <td>
+        {activated ? (
+          <span className="text-green">
+            <SuccessIcon />{' '}
+          </span>
+        ) : (
+          <span className="text-muted">
+            <InfoIcon />
+          </span>
+        )}
+      </td>
+      <td>
+        {userRole === 1 && (
+          <button
+            className="btn btn-sm btn-secondary"
+            onClick={processRoleChange}
+          >
+            Upgrade to an Editor
+          </button>
+        )}
+        {userRole === 3 && (
+          <button
+            className="btn btn-sm btn-danger"
+            // to={`/admin/user/${_id}`}
+            onClick={processRoleChange}
+          >
+            Downgrade to a User
+          </button>
+        )}
+      </td>
+    </tr>
+  );
+};
 
 export default Users;
